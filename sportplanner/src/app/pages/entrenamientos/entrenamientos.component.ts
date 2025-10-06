a603ed859f5ec3bfea33d4495c5b5b724929ecc8
Mursito1 <deltamur@hotmail.com>
2025-10-06 12:38:23 -0300

    Creacion páginas

---
+++ sportplanner/src/app/pages/entrenamientos/entrenamientos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entrenamientos',
+  templateUrl: './entrenamientos.component.html',
+  styleUrls: ['./entrenamientos.component.scss'],
+  standalone: true,
+})
+export class EntrenamientosComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
